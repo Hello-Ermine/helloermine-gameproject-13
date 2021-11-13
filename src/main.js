2124e5890330cd96f69c3a0e98842faae9ea25b6
JoNoteJoMaMa <notnot45@hotmail.com>
2021-11-13 15:11:43 +0700

    ARghhhh

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const config = {
     pixelArt: true,
     roundPixels: true,
     parent: 'content',
-    width: 450,
+    width: 800,
     height: 720,
     physics: {
         default: 'arcade',
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,22 @@
 import Phaser from "phaser";
 
+let bg;
+let player;
+let wall;
+let music;
+let running;
+let soundButton;
+let menuButton;
+let cross;
+
+let profile;
+let menu;
+
+let keyW;
+let keyA;
+let keyS;
+let keyD;
+
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -9,13 +26,87 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         
+
+        this.load.spritesheet('player','src/image/runningErmine.png',
+        { frameWidth: 242 , frameHeight: 266});
+
+
+
     }
 
     create() {
         
-    }
 
+        player = this.physics.add.sprite(200, 450, 'player').setScale(0.5);
+
+        this.anims.create({
+            key: 'playerAni',
+            frames: this.anims.generateFrameNumbers('player', {
+                start: 0,
+                end: 3
+            }),
+            duration: 500,    
+            repeat: -1
+        })
+
+        
+
+        // soundButton = this.add.image(410, 100, 'sound').setScale(0.2).setDepth(1);
+        // soundButton.setInteractive();
+        // soundButton.on('pointerup',()=>{
+        //     if(!this.sound.mute){
+        //         this.sound.mute = true;
+        //         cross.setVisible(true);
+        //     }else{
+        //         this.sound.mute = false;
+        //         cross.setVisible(false);
+        //     }             
+        // })
+        
+        
+
+        // menuButton = this.add.image(410, 50, 'exit').setScale(0.3);
+        // menuButton.setInteractive();
+        // menuButton.on('pointerup',()=>{
+        //     this.scene.start('MainMenu');
+        //     music.stop(); 
+        //     running.stop();          
+        // })
+        
+
+
+
+        //Player Control
+    keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    
+
+    player.setCollideWorldBounds(true) 
+    this.physics.add.collider(player, wall);
+
+    
+    }
     update(delta, time) {
+
+        player.anims.play('playerAni', true);
+
+
+        if(keyW.isDown){
+            player.setVelocityY(-500);
+        }else if(keyS.isDown){
+            player.setVelocityY(500);
+        }else{
+            player.setVelocityY(0);
+        }
+        if(keyA.isDown){
+            player.setVelocityX(-500);
+        }else if(keyD.isDown){
+            player.setVelocityX(500);
+        }else{
+            player.setVelocityX(0);
+        }
         
     }
 }
