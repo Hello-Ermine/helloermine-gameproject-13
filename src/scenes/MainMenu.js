c2b5ed2079d6f25d6797ded95a110a24ad0ea2ee
Mxoxoxk <Parisara.mxoxoxk@mail.kmutt.ac.th>
2021-11-18 19:38:57 +0700

    menu button
    เปลี่ยนสีปุ่มเมื่อลากเมาส์ผ่าน

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -26,24 +26,44 @@ class MainMenu extends Phaser.Scene {
 
         background = this.add.image(640, 360, 'bg').setDepth(5);
         
-       //Menu Button
+       //Play
         playButton = this.physics.add.image(240, 380, 'play').setScale(0.5).setDepth(10);
         playButton.setInteractive();
         playButton.on('pointerdown', () => {
             this.scene.start('GameScene')
-         })
-
+        });
+        playButton.on('pointerover', function () {
+            playButton.setTint(0x9acd32);
+        });
+        playButton.on('pointerout', function () {
+            playButton.clearTint();
+        });    
+        
+        //tutorial
         tutorialButton = this.physics.add.image(240, 500, 'tutorial').setScale(0.5).setDepth(10);
         tutorialButton.setInteractive();
         tutorialButton.on('pointerdown', () => {
             this.scene.start('TutorialScene')
-        })
-
+        });
+        tutorialButton.on('pointerover', function () {
+            tutorialButton.setTint(0x9acd32);
+        });
+        tutorialButton.on('pointerout', function () {
+            tutorialButton.clearTint();
+        }); 
+        
+        //credit
         creditButton = this.physics.add.image(240, 620, 'credit').setScale(0.5).setDepth(10);
         creditButton.setInteractive();
         creditButton.on('pointerdown', () => {
             this.scene.start('CreditScene')
-        })
+        });
+        creditButton.on('pointerover', function () {
+            creditButton.setTint(0x9acd32);
+        });
+        creditButton.on('pointerout', function () {
+            creditButton.clearTint();
+        }); 
     }
 
     update(delta, time) {
